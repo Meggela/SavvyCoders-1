ecb33a291ffcbf696464fc497bab1ad8daad64ca
Meg Shelley <megshelle@ymail.com>
2017-10-11 23:09:38 -0500

    Add photo properly to theme images folder

--- blog/script.js
+++ blog/script.js
@@ -1,5 +1,6 @@
 /* globals $, _ */
 
+var host =
 var postRequest = $.ajax( "https://jsonplaceholder.typicode.com/posts" );
 var $posts = $( "#posts" );
 
@@ -15,14 +16,22 @@ function templatePost( post ){
 }
 
 function appendPosts( posts ){
-    var firstFive = _
-        .take( posts, 5 )
+    var firstFive = _.take( posts, 5 );
+    var requests = firstFive.reduce( getAuthor, {} );
+    var markup = firstFive.reduce( templatePost, "" );
+
+    var requestPromises = Object.values( requests );
+
         .reduce(
             ( output, post ) => output + templatePost( post ),
             ""
         );
 
-    $posts.append( firstFive );
+    $posts.append( markup );
+
+    Promise
+        .all( requestPromises )
+        .then( injectAuthors );
 }
 
 postRequest.then( appendPosts );
